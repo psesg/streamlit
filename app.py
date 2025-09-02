d135d64650d1f14ccdc7389d976441fb9d88bd31
Serg <pseesp@mail.ru>
2025-09-02 21:43:44 +0300

    +key1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # https://psesg-streamlit-app-vy2pzg.streamlit.app/
 api_key = st.secrets["my_api_key"]
-st.write(f"Hello world: {api_key}")
+st.write(f"Hello streamlit: {api_key}")
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
