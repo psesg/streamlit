7d4ff4777e473fe32eb6bc0276083adffc1f9099
Serg <pseesp@mail.ru>
2025-09-02 21:50:15 +0300

    +key from st.secrets

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+'''
+    This is a sample app to test the streamlit app
+'''
 import streamlit as st
+
 # https://psesg-streamlit-app-vy2pzg.streamlit.app/
 api_key = st.secrets["my_api_key"]
 st.write(f"Hello streamlit: {api_key}")
